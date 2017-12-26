444ffa13dd9fccbef5c1dc49875acdc4e5727754
GrimLefourbe <vivienrobertbarbe@gmail.com>
2017-12-26 19:50:03 +0100

    Package structure defined. Actions sorting done. Test functions

--- packagemanager/package_manager.py
+++ packagemanager/package_manager.py
@@ -1,5 +1,113 @@
+import logging
 
+from typing import Dict, Tuple, List, Any, Union, Callable
+
+
+# requirements = List[str] #list of ids of the packages required for the current package
+# conflicts = List[str] #list of ids of packages incompatible with the current one
+# before = List[str] #list of ids of packages that must be installed before the current package
+# after = List[str] #list of ids of packages that must be installed after the current package
+# dependencies = Tuple[requirements, conflicts, before, after]
+
+
+
+class Component:
+    """
+
+    """
+
+    def __init__(self, componentid: str, componentname: str, subcomponents: List["Component"]):
+        self.id = componentid
+        self.name = componentname
+        self.subcomponents = subcomponents
+
+
+class Dependencies:
+    """
+
+    """
+
+    def __init(self, requirements: List[str] = None, conflicts: List[str] = None, before: List[str] = None,
+               after: List[str] = None):
+        self.requirements = requirements if requirements else []
+        self.conflicts = conflicts if conflicts else []
+        self.before = before if before else []
+        self.after = after if after else []
 
 class Package:
-    def __init__(self):
-        pass
+    """
+
+    """
+
+    def __init__(self, packageid: str, name: str, depends: List[Tuple[Dependencies, List[str]]],
+                 components: List[Component]):
+        """
+        This initialises a package with an id and a name. The id will be used as a unique identifier. The name will be displayed to the user.
+        components must describe the components of the package.
+        depends is a list of tuples. Each tuple must have dependencies as a first element and a list of the component ids they apply to as second element.
+        :param id: Unique identifier of the package
+        :param name: Name of the package
+        :param depends: dependencies of the package
+        :param components: components of the package
+        """
+        self.id = packageid
+        self.name = name
+        self.depends = depends
+        self.components = components
+
+
+class InstallAction:
+    """
+
+    """
+
+    def __init__(self, installmethod: Callable, prev: List["InstallAction"], name: str):
+        self.execute = installmethod
+        self.prev = prev
+        self.name = name
+
+
+class IncompatibleActionsException(Exception):
+    pass
+
+
+def sortInstallActionList(actions: List[InstallAction]) -> List[InstallAction]:
+    log = logging.getLogger(__name__)
+    # depth first search into topologic sort
+    sortedList = []
+
+    def depthFirstSort(action):
+
+        # Open node
+        if action.state == 1:
+            log.error(
+                'There is a loop in the action list, the action {} is within its predecessors'.format(action.name))
+            raise IncompatibleActionsException('There is a loop in the ')
+
+        # Closed node
+        if action.state == 2:
+            log.debug('Action {} is closed, nothing new here'.format(action.name))
+            return
+
+        # New node
+        log.debug('Opening {}'.format(action.name))
+        action.state = 1
+        for prevAction in action.prev:
+            log.debug('Looking at child {}'.format(action.name))
+            depthFirstSort(prevAction)
+        log.debug('Closing {}'.format(action.name))
+        action.state = 2
+        sortedList.append(action)
+
+    for action in actions:
+        action.state = 0
+
+    for action in actions:
+        log.debug('Initialising search on action {}'.format(action.name))
+        # noinspection PyTypeChecker
+        depthFirstSort(action)
+
+    for action in actions:
+        del action.state
+
+    return sortedList
--- test.py
+++ test.py
@@ -7,6 +7,72 @@ logging.config.dictConfig(json.load(open('logging_config.json', 'r')))
 import packagemanager as pm
 
 
+def test_package():
+    log.info('Starting package test')
+    c1 = pm.Component(componentid="cid01", componentname="name01", subcomponents=[])
+    log.debug('c1 created')
+    p1 = pm.Package(packageid="pid01", name="name02", depends=[(pm.Dependencies(), ["all"])], components=[c1])
+    log.debug('p1 created')
+    log.info('Simple package creation tested')
+
+    c11 = pm.Component(componentid="cid02", componentname="name02", subcomponents=[])
+    log.debug('c11 created')
+    c12 = pm.Component(componentid="cid03", componentname="name03", subcomponents=[])
+    log.debug('c12 created')
+    c13 = pm.Component(componentid="cid04", componentname="name04", subcomponents=[c11, c12])
+    log.debug('c13 created')
+    c14 = pm.Component(componentid="cid05", componentname="name05", subcomponents=[])
+    log.debug('c14 created')
+    c15 = pm.Component(componentid="cid06", componentname="name06", subcomponents=[c14])
+    log.debug('c15 created')
+
+    p2 = pm.Package(packageid="pid02", name="name07", depends=[(pm.Dependencies(), ["all"])], components=[c13, c15])
+    log.debug('p2 created')
+
+    log.info('Complex package creation tested')
+    log.info('Done testing package')
+
+
+def test_action_sort():
+    log.info('Starting action sort test')
+
+    def dummy_installmethod():
+        pass
+
+    down = pm.InstallAction(installmethod=dummy_installmethod, prev=[], name='Download')
+    ext = pm.InstallAction(installmethod=dummy_installmethod, prev=[down], name='Extraction')
+
+    m11 = pm.InstallAction(installmethod=dummy_installmethod, prev=[], name='m11')
+    log.debug('m11 created')
+    m12 = pm.InstallAction(installmethod=dummy_installmethod, prev=[m11, down], name='m12')
+    log.debug('m12 created')
+    m13 = pm.InstallAction(installmethod=dummy_installmethod, prev=[m12, ext], name='m13')
+    log.debug('m13 created')
+    m21 = pm.InstallAction(installmethod=dummy_installmethod, prev=[], name='m21')
+    log.debug('m21 created')
+    m22 = pm.InstallAction(installmethod=dummy_installmethod, prev=[m21, down], name='m22')
+    log.debug('m22 created')
+    m23 = pm.InstallAction(installmethod=dummy_installmethod, prev=[m13, m22, ext], name='m23')
+    log.debug('m23 created')
+
+    down.prev.extend([m11, m21])
+    ext.prev.extend([m12, m22])
+
+    actions = [down, ext, m23, m22, m21, m13, m12, m11]
+
+    sortedActions = pm.sortInstallActionList(actions)
+
+    log.debug('sorted actions : {}'.format([a.name for a in sortedActions]))
+
+    m22.prev.append(m23)
+
+    try:
+        log.error("Incompatible actions aren't detected".format(pm.sortInstallActionList(actions)))
+    except pm.IncompatibleActionsException:
+        log.info('Incompatible actions detection successful')
+    log.info('Done testing action sort')
+
 if __name__ == '__main__':
     log.info('Starting tests')
-    c = pm.Package()
+    test_package()
+    test_action_sort()
