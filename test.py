f17deb4a1b12ce2cc7d4c1e37a5b341206b8d831
Vivien <vivienrobertbarbe@gmail.com>
2018-01-03 01:42:32 +0100

    Reworked tests into unit tests

--- test.py
+++ test.py
@@ -9,192 +9,137 @@ logging.config.dictConfig(json.load(open('logging_config.json', 'r')))
 import packagemanager as pm
 
 
-def test_components():
-    log.info('Starting component test')
-    c1 = pm.Component(componentid='cid01', componentname="name01", subcomponents=[])
-    log.debug('c1 created')
-    log.info('Simple component creation tested')
-    c1dict = c1.to_dict()
-    try:
-        assert c1dict == {'id': "cid01", 'name': 'name01', 'subcomponents': []}
-        log.debug('c1 dict is as expected')
-    except AssertionError:
-        log.error('c1 dict is not as expected. c1dict : {}'.format(c1dict))
-    log.info('Simple component export tested')
-    log.info('Simple component tested')
-    c11 = pm.Component(componentid="cid01", componentname="name01", subcomponents=[])
-    log.debug('c11 created')
-    c12 = pm.Component(componentid="cid02", componentname="name02", subcomponents=[])
-    log.debug('c12 created')
-    c13 = pm.Component(componentid="cid03", componentname="name03", subcomponents=[c11, c12])
-    log.debug('c13 created')
-    c14 = pm.Component(componentid="cid04", componentname="name04", subcomponents=[])
-    log.debug('c14 created')
-    c15 = pm.Component(componentid="cid05", componentname="name05", subcomponents=[c14])
-    log.debug('c15 created')
-    c16 = pm.Component(componentid="cid06", componentname="name06", subcomponents=[c13, c15])
-    log.debug('c16 created')
+class TestComponents(unittest.TestCase):
+    def setUp(self):
+        self.c1 = pm.Component(componentid='cid01', componentname='name01', subcomponents=[])
+        self.c2 = pm.Component(componentid='cid02', componentname='name02', subcomponents=[])
+        self.c3 = pm.Component(componentid='cid03', componentname='name03', subcomponents=[])
+        self.c4 = pm.Component(componentid='cid04', componentname='name04', subcomponents=[self.c2, self.c3])
+        self.c5 = pm.Component(componentid='cid05', componentname='name05', subcomponents=[])
+        self.c6 = pm.Component(componentid='cid06', componentname='name06', subcomponents=[self.c5])
+        self.c7 = pm.Component(componentid='cid07', componentname='name07', subcomponents=[self.c4, self.c6])
 
-    log.info('Complex component creation tested')
+    def test_to_dict_simple(self):
+        expected = {'id': 'cid01',
+                    'name': 'name01',
+                    'subcomponents': []}
+        self.assertEqual(self.c1.to_dict(), expected)
 
-    c16dict = c16.to_dict()
-    try:
-        assert c16dict == {'id': "cid06",
-                           'name': "name06",
-                           'subcomponents': [
-                               {'id': 'cid03',
-                                'name': 'name03',
-                                'subcomponents': [
-                                    {'id': 'cid01',
-                                     'name': 'name01',
-                                     'subcomponents': []},
-                                    {'id': 'cid02',
-                                     'name': 'name02',
-                                     'subcomponents': []}]},
-                               {'id': 'cid05',
-                                'name': 'name05',
-                                'subcomponents': [
-                                    {'id': 'cid04',
-                                     'name': 'name04',
-                                     'subcomponents': []}]}]}
-        log.debug('c16 is as expected')
-    except AssertionError:
-        log.error('c16 dict is not as expected. c16dict : {}'.format(c16dict))
-    log.info('Complex component export tested')
-    log.info('Complex component tested')
-    log.info('Done testing components')
+    def test_to_dict_complex(self):
+        expected = {'id': "cid07",
+                    'name': "name07",
+                    'subcomponents': [
+                        {'id': 'cid04',
+                         'name': 'name04',
+                         'subcomponents': [
+                             {'id': 'cid02',
+                              'name': 'name02',
+                              'subcomponents': []},
+                             {'id': 'cid03',
+                              'name': 'name03',
+                              'subcomponents': []}]},
+                        {'id': 'cid06',
+                         'name': 'name06',
+                         'subcomponents': [
+                             {'id': 'cid05',
+                              'name': 'name05',
+                              'subcomponents': []}]}]}
+        self.assertEqual(self.c7.to_dict(), expected)
 
 
-def test_package():
-    log.info('Starting package test')
-    c1 = pm.Component(componentid="cid01", componentname="name01", subcomponents=[])
-    log.debug('c1 created')
-    p1 = pm.Package(packageid="pid01", name="name02", depends=[(pm.Dependencies(), ["all"])], components=[c1])
-    log.debug('p1 created')
-    log.info('Simple package creation tested')
-    p1dict = p1.to_dict()
-    try:
-        assert p1dict == {'id': 'pid01',
-                          'name': 'name02',
-                          'dependencies': [
-                              {'components': ['all'],
-                               'dep': {'requirements': [],
-                                       'conflicts': [],
-                                       'before': [],
-                                       'after': []}}],
-                          'components': [{'id': "cid01",
-                                          'name': 'name01',
-                                          'subcomponents': []}]}
-        log.debug('p1dict is as expected')
-    except AssertionError:
-        log.error('p1dict is not as expected. p1dict : {}'.format(p1dict))
-    log.debug('testing export to json')
-    pm.Package.save_to_json({'pid01': p1}, 'test.json')
-    log.info('Simple package export tested')
-    p1reconst = pm.Package.load_from_json('test.json')
+class TestPackage(unittest.TestCase):
+    def setUp(self):
+        self.c1 = pm.Component(componentid='cid01', componentname='namec01', subcomponents=[])
+        self.p1 = pm.Package(packageid='pid01', name='namep01', depends=[(pm.Dependencies(), ['all'])],
+                             components=[self.c1])
 
-    try:
-        assert p1reconst['pid01'].to_dict() == p1.to_dict()
-        log.debug('p1reconst is equal to p1')
-    except AssertionError:
-        log.error('p1reconst is not equal to p1')
-    log.info('simple package import tested')
-    log.info('Simple package tested')
+        self.c2 = pm.Component(componentid='cid02', componentname='namec02', subcomponents=[])
+        self.c3 = pm.Component(componentid='cid03', componentname='namec03', subcomponents=[])
+        self.c4 = pm.Component(componentid='cid04', componentname='namec04', subcomponents=[self.c2, self.c3])
+        self.c5 = pm.Component(componentid='cid05', componentname='namec05', subcomponents=[])
+        self.c6 = pm.Component(componentid='cid06', componentname='namec06', subcomponents=[self.c5])
 
-    c11 = pm.Component(componentid="cid02", componentname="name02", subcomponents=[])
-    log.debug('c11 created')
-    c12 = pm.Component(componentid="cid03", componentname="name03", subcomponents=[])
-    log.debug('c12 created')
-    c13 = pm.Component(componentid="cid04", componentname="name04", subcomponents=[c11, c12])
-    log.debug('c13 created')
-    c14 = pm.Component(componentid="cid05", componentname="name05", subcomponents=[])
-    log.debug('c14 created')
-    c15 = pm.Component(componentid="cid06", componentname="name06", subcomponents=[c14])
-    log.debug('c15 created')
+        self.p2 = pm.Package(packageid="pid02", name="namep02", depends=[(pm.Dependencies(), ["all"])],
+                             components=[self.c4, self.c6])
 
-    p2 = pm.Package(packageid="pid02", name="name07", depends=[(pm.Dependencies(), ["all"])], components=[c13, c15])
-    log.debug('p2 created')
+    def test_to_dict_simple(self):
+        expected = {'id': 'pid01',
+                    'name': 'namep01',
+                    'dependencies': [
+                        {'involvedcomps': ['all'],
+                         'dependency': {'requirements': [],
+                                        'conflicts': [],
+                                        'before': [],
+                                        'after': []}}],
+                    'components': [{'id': "cid01",
+                                    'name': 'namec01',
+                                    'subcomponents': []}]}
+        self.assertEqual(self.p1.to_dict(), expected)
 
-    log.info('Complex package creation tested')
+    def test_to_dict_complex(self):
+        expected = {'id': 'pid02',
+                    'name': 'namep02',
+                    'dependencies': [
+                        {'involvedcomps': ['all'],
+                         'dependency': {'requirements': [],
+                                        'conflicts': [],
+                                        'before': [],
+                                        'after': []}}],
+                    'components': [{'id': 'cid04',
+                                    'name': 'namec04',
+                                    'subcomponents': [{'id': 'cid02',
+                                                       'name': 'namec02',
+                                                       'subcomponents': []},
+                                                      {'id': 'cid03',
+                                                       'name': 'namec03',
+                                                       'subcomponents': []}]},
+                                   {'id': 'cid06',
+                                    'name': 'namec06',
+                                    'subcomponents': [{'id': 'cid05',
+                                                       'name': 'namec05',
+                                                       'subcomponents': []}]}]}
+        self.assertEqual(self.p2.to_dict(), expected)
 
-    p2dict = p2.to_dict()
-    try:
-        assert p2dict == {'id': 'pid02',
-                          'name': 'name07',
-                          'dependencies': [
-                              {'components': ['all'],
-                               'dep': {'requirements': [],
-                                       'conflicts': [],
-                                       'before': [],
-                                       'after': []}}],
-                          'components': [{'id': 'cid04',
-                                          'name': 'name04',
-                                          'subcomponents': [{'id': 'cid02',
-                                                             'name': 'name02',
-                                                             'subcomponents': []},
-                                                            {'id': 'cid03',
-                                                             'name': 'name03',
-                                                             'subcomponents': []}]},
-                                         {'id': 'cid06',
-                                          'name': 'name06',
-                                          'subcomponents': [{'id': 'cid05',
-                                                             'name': 'name05',
-                                                             'subcomponents': []}]}]}
-        log.debug('p2 dict is as expected')
-    except AssertionError:
-        log.error('p2 dict is not as expected. p2dict : {}'.format(p2dict))
-    log.debug('testing export to json')
-    p2.save_to_json({'pid01': p1, 'pid02': p2}, 'test.json')
-    log.info('Complex package export tested')
-    log.info('Complex package tested')
-    log.info('Done testing package')
+    def test_import_export(self):
+        pkgdict = {self.p1.id: self.p1, self.p2.id: self.p2}
+        pm.Package.save_to_json({self.p1.id: self.p1, self.p2.id: self.p2}, 'test.json')
+        pkgdictexp = pm.Package.load_from_json('test.json')
+        self.assertEqual({pkg.id: pkg.to_dict() for pkg in pkgdict.values()},
+                         {pkg.id: pkg.to_dict() for pkg in pkgdictexp.values()})
 
 
+class TestActionSort(unittest.TestCase):
+    def setUp(self):
+        def dummy_installmethod():
+            pass
 
-def test_action_sort():
-    log.info('Starting action sort test')
+        self.down = pm.InstallAction(installmethod=dummy_installmethod, prev=[], name='Download')
+        self.ext = pm.InstallAction(installmethod=dummy_installmethod, prev=[self.down], name='Extraction')
 
-    def dummy_installmethod():
-        pass
+        self.m11 = pm.InstallAction(installmethod=dummy_installmethod, prev=[], name='m11')
+        self.m12 = pm.InstallAction(installmethod=dummy_installmethod, prev=[self.m11, self.down], name='m12')
+        self.m13 = pm.InstallAction(installmethod=dummy_installmethod, prev=[self.m12, self.ext], name='m13')
+        self.m21 = pm.InstallAction(installmethod=dummy_installmethod, prev=[], name='m21')
+        self.m22 = pm.InstallAction(installmethod=dummy_installmethod, prev=[self.m21, self.down], name='m22')
+        self.m23 = pm.InstallAction(installmethod=dummy_installmethod, prev=[self.m13, self.m22, self.ext], name='m23')
 
-    down = pm.InstallAction(installmethod=dummy_installmethod, prev=[], name='Download')
-    ext = pm.InstallAction(installmethod=dummy_installmethod, prev=[down], name='Extraction')
+        self.down.prev.extend([self.m11, self.m21])
+        self.ext.prev.extend([self.m12, self.m22])
 
-    m11 = pm.InstallAction(installmethod=dummy_installmethod, prev=[], name='m11')
-    log.debug('m11 created')
-    m12 = pm.InstallAction(installmethod=dummy_installmethod, prev=[m11, down], name='m12')
-    log.debug('m12 created')
-    m13 = pm.InstallAction(installmethod=dummy_installmethod, prev=[m12, ext], name='m13')
-    log.debug('m13 created')
-    m21 = pm.InstallAction(installmethod=dummy_installmethod, prev=[], name='m21')
-    log.debug('m21 created')
-    m22 = pm.InstallAction(installmethod=dummy_installmethod, prev=[m21, down], name='m22')
-    log.debug('m22 created')
-    m23 = pm.InstallAction(installmethod=dummy_installmethod, prev=[m13, m22, ext], name='m23')
-    log.debug('m23 created')
+        self.actions = [self.down, self.ext, self.m23, self.m22, self.m21, self.m13, self.m12, self.m11]
 
-    down.prev.extend([m11, m21])
-    ext.prev.extend([m12, m22])
+    def test_sort(self):
+        sortedActions = pm.sortInstallActionList(self.actions)
 
-    actions = [down, ext, m23, m22, m21, m13, m12, m11]
+        for action in self.actions:
+            for prevaction in action.prev:
+                with self.subTest(i=(action, prevaction)):
+                    self.assertGreater(sortedActions.index(action), sortedActions.index(prevaction))
 
-    sortedActions = pm.sortInstallActionList(actions)
-
-    log.debug('sorted actions : {}'.format([a.name for a in sortedActions]))
-
-    m22.prev.append(m23)
-
-    try:
-        log.error("Incompatible actions aren't detected".format(pm.sortInstallActionList(actions)))
-    except pm.IncompatibleActionsException:
-        log.info('Incompatible actions detection successful')
-    log.info('Done testing action sort')
+    def test_loop_detection(self):
+        self.m22.prev.append(self.m23)
+        self.assertRaises(pm.IncompatibleActionsException)
 
 if __name__ == '__main__':
     log.info('Starting tests')
-    try:
-        test_components()
-        test_package()
-        test_action_sort()
-    except Exception:
-        log.exception('Unkown Error')
+    unittest.main()
